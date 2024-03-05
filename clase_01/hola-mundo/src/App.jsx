a0ed7bb6f2a0004d1c86a6e6236db3d60ad4f4dc
diegofdg <diegofdg@gmail.com>
2024-03-05 18:28:18 -0300

    creando tu primer proyecto práctico con react

---
+++ clase_01/hola-mundo/src/App.jsx
@@ -0,0 +1,20 @@
+export function App() {
+    return (
+        <article>
+            <header>
+                <img
+                    alt="El avatar del midudev"
+                    src="https://unavatar.io/midudev"/>
+                <div>
+                    <strong>Miguel Angel Durán</strong>
+                    <span>@midudev</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}

df22cf1fe4dcb98d70da1e1bee0fd30892ba36dc
diegofdg <diegofdg@gmail.com>
2024-03-05 19:22:45 -0300

    estila contenedor para separaciones y no componentes-pasando booleanos como props

--- clase_01/hola-mundo/src/App.jsx
+++ clase_01/hola-mundo/src/App.jsx
@@ -3,23 +3,27 @@ import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App() {
     return (
-        <>
+        <section className='App'>
             <TwitterFollowCard
+                isFollowing
                 userName={"midudev"}
                 name={"Miguel Angel Duran"}
             />
             <TwitterFollowCard
+                isFollowing={false}
                 userName="pheralb"
                 name={"Pablo Hernandez"}
             />
             <TwitterFollowCard
+                isFollowing={false}
                 userName="elonmusk"
                 name={"Elon Musk"}
             />
             <TwitterFollowCard
+                isFollowing
                 userName="vxnder"
                 name={"Vanderhart"}
             />
-        </>
+        </section>
     )
 }
